435fad049063b0eadc76b3c01b8cfa395d50acde
Ping Li <imuliping@gmail.com>
2020-10-12 23:17:34 -0400

    this is a test

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 if (true) {
     brick.setStatusLight(StatusLight.Orange)
 } else {
-	
+    MapleRobot.colorMove(
+    movingSideEnum.LonL,
+    stopCondColorEnum.LINE_STOP,
+    15,
+    80
+    )
 }
 forever(function () {
 	
